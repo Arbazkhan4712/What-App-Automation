f0372be631726a22956aa3ddc9423efc002b1c6f
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-03-22 23:30:27 +0530

    updated script

--- whats_app_ubuntu.py
+++ whats_app_ubuntu.py
@@ -11,31 +11,30 @@ driver.get("https://web.whatsapp.com/")
 
 #send message function
 def Sendmsg():
-    
+
     #enter name of receiver
-    name = raw_input("Enter the name of user or group : ")
-    #enter the message 
-    msg = raw_input("Enter the message : ")
+    name = input("Enter the name of user or group : ")
+    #enter the message
+    msg = input("Enter the message : ")
     #enter the count
     count = int(input("Enter Number of count : "))
 
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     user.click()
 
-    msg_box = driver.find_element_by_class_name("_3u328")
+    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
 
     for i in range(count):
         msg_box.send_keys(msg)
-        button = driver.find_element_by_class_name("_3M-N-")
+        button = driver.find_element_by_xpaths('//*[@id="main"]/footer/div[1]/div[3]')
         button.click()
 
 #send image or video file function
 def sendimgvid():
-    
     #enter name of receiver
-    name = raw_input("Enter the name of user or group : ")
+    name = input("Enter the name of user or group : ")
     #enter file path
-    filepath = raw_input("Enter the file path (Image,Video) : ")
+    filepath = input("Enter the file path (Image,Video) : ")
 
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     user.click()
@@ -62,4 +61,3 @@ elif(n == 2):
 
 elif(n==3):
     quit()
-
